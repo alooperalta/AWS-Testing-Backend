ed1c6673956553c7b039e9e1b38a3ed6a1ad0d71
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2022-07-11 13:18:38 +0530

    added sime keys and middleware

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 console.log("Backend Started .......");
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const mognodb =     require('mongodb');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-
+const mongoose = require("mongoose");
+const cors = require("cors");
+const mognodb = require("mongodb");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
 // dotenv.config();
 dotenv.config();
@@ -15,25 +14,23 @@ dotenv.config();
 app.use(cors());
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 //bodyparser config
 app.use(bodyParser.json());
 
 const port = process.env.PORT;
 
-
-
 //connecting the mongoose
-mongoose.connect(process.env.DB_CONNECTION, ()=>
-{
-    console.log("MongoDB Connected");
-})
+mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log("MongoDB Connected");
+});
 
 //app file
-app.get("/", (req, res) => {res.send("Server Started...")});
-
+app.get("/", (req, res) => {
+  res.send("Server Started...");
+});
 
 //listening the port
 app.listen(port, () => {
-    console.log(`Server Started at port ${port}`);
+  console.log(`Server Started at port ${port}`);
 });
